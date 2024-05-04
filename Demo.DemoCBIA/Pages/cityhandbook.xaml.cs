246298463e523509234f29d6b5ffe7f490f68452
aj0325 <133852566+aj0325@users.noreply.github.com>
2024-05-04 14:07:32 +0530

    Update cityhandbook.xaml.cs

--- Demo.DemoCBIA/Pages/cityhandbook.xaml.cs
+++ Demo.DemoCBIA/Pages/cityhandbook.xaml.cs
@@ -7,17 +7,5 @@ public partial class cityhandbook : ContentPage
 	public cityhandbook()
 	{
 		InitializeComponent();
-#if ANDROID
-        Microsoft.Maui.Handlers.WebViewHandler.Mapper.AppendToMapping("pdfviewer", (handler, View) =>
-        {
-            handler.PlatformView.Settings.AllowFileAccess = true;
-            handler.PlatformView.Settings.AllowFileAccessFromFileURLs = true;
-            handler.PlatformView.Settings.AllowUniversalAccessFromFileURLs = true;
-        });
-
-        pdfview.Source = $"file:///android_asset/pdfjs/web/viewer.html?file=file:///android_asset/{WebUtility.UrlEncode("mumbai_handbook.pdf")}";
-#else
-        pdfview.Source = "mumbai_handbook.pdf";
-#endif
     }
 }
